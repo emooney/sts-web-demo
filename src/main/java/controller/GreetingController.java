e91bf3aed33c5b5592e63ea6b265b5349b392b5f
Eric Mooney <eric.mooney@gmail.com>
2016-11-20 23:48:03 -0500

    added ssn form

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -7,9 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import model.Greeting;
 
@@ -17,12 +14,7 @@ import model.Greeting;
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	
-//	@RequestMapping("/greeting")
-//	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-//		
-//		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-//	}
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("greeting", new Greeting());
